16793d14f942a29231554a5b507765863d3e2f15
mainangethe <ngethe@mradi.co.ke>
2019-02-25 10:14:25 +0300

    Add dynamic grid generation
    Added an event listener on the pixel-selector, which removes current
    grid and adds a new one based on the new value provided.
    
    JIRA: FEDT-52

--- scripts.js
+++ scripts.js
@@ -1,17 +1,51 @@
+let eventFire = false
+let defaultSize;
+const pixelSelector = document.getElementById('pixel-selector');
 const gridContainer = document.querySelector('.grid-container');
-const gridBox = document.createElement('div');
 
-let defaultSize = 64;
-let screenWidth =  gridContainer.offsetWidth;
-let screenHeight = gridContainer.offsetHeight;
-let gridBoxCount = Math.floor((screenWidth * screenHeight) / (defaultSize * defaultSize));
-
-for (let i = 0; i < gridBoxCount; i++) {
-    const gridBox = document.createElement('div');
-    gridBox.classList.add('grid');
-    gridBox.style.width = defaultSize + 'px';
-    gridBox.style.height = defaultSize + 'px';
-    gridContainer.appendChild(gridBox);
+function createGrid(gridSize) {
+  const gridBox = document.createElement('div');
+  let screenWidth =  gridContainer.offsetWidth;
+  let screenHeight = gridContainer.offsetHeight;
+  let gridBoxCount = Math.floor((screenWidth * screenHeight) / (gridSize * gridSize));
+  for (let i = 0; i < gridBoxCount; i++) {
+      const gridBox = document.createElement('div');
+      gridBox.classList.add('grid');
+      gridBox.style.width = gridSize + 'px';
+      gridBox.style.height = gridSize + 'px';
+      gridContainer.appendChild(gridBox);
+  }
+  eventFire = false;
 }
 
-console.log(screenHeight);
+function removeGrid() {
+  const divs = document.getElementsByClassName('grid');
+  let divsArray = Array.from(divs);
+  for(let div of divsArray) {
+    gridContainer.removeChild(div);
+  }
+}
+
+function getScreenPixelSize() {
+  let pixelSize;
+  if (eventFire == false) pixelSize = 16;
+  else {
+    const pixelContainer = document.querySelector('.pixel-selector-container');
+    let pixelOutput = document.getElementById('pixel-output');
+    pixelSize = pixelSelector.value;
+    pixelOutput.textContent = pixelSize + 'px';
+    pixelContainer.appendChild(pixelOutput);
+  }
+    return pixelSize;
+}
+
+pixelSelector.addEventListener('change', () => {
+  eventFire = true;
+  defaultSize = getScreenPixelSize();
+  console.log('event fired. New pixel size: ', defaultSize, eventFire);
+  removeGrid();
+  createGrid(defaultSize);
+  console.log('new grid created');
+});
+
+createGrid(getScreenPixelSize());
