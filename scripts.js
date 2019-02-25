bdf9bc935af7946337033692d79d9393ebc4c996
mainangethe <ngethe@mradi.co.ke>
2019-02-25 20:33:33 +0300

    Add multi-color options
    This feature allows a user to draw over the screen with multiple colors
    in random.
    
    JIRA: FEDT-52

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
-let eventFire = false
-let defaultSize;
 const pixelSelector = document.getElementById('pixel-selector');
 const gridContainer = document.querySelector('.grid-container');
+const eraserButton = document.querySelector('#screen-eraser');
+const multiColorsCheck = document.querySelector('#color-picker');
+const MULTI_COLOURS = [
+  'hsl(180, 100%, 25%)',
+  'hsl(243, 100%, 13%)',
+  'hsl(134, 90%, 50%)',
+  'hsl(46, 100%, 50%)',
+  'hsl(343, 100%, 43%)',
+];
 
+
+let eventFire = false;
+let defaultSize;
+let divsArray;
 function createGrid(gridSize) {
   const gridBox = document.createElement('div');
   let screenWidth =  gridContainer.offsetWidth;
@@ -19,9 +30,8 @@ function createGrid(gridSize) {
 }
 
 function removeGrid() {
-  const divs = document.getElementsByClassName('grid');
-  let divsArray = Array.from(divs);
-  for(let div of divsArray) {
+  divsArray = getAllDivs();
+  for (let div of divsArray) {
     gridContainer.removeChild(div);
   }
 }
@@ -39,6 +49,18 @@ function getScreenPixelSize() {
     return pixelSize;
 }
 
+function clearScreen() {
+  divsArray = getAllDivs();
+  for (let div of divsArray) {
+    div.style.backgroundColor = '';
+  }
+}
+
+function getAllDivs() {
+  const divs = document.getElementsByClassName('grid');
+  return Array.from(divs);
+}
+
 pixelSelector.addEventListener('change', () => {
   eventFire = true;
   defaultSize = getScreenPixelSize();
@@ -47,12 +69,25 @@ pixelSelector.addEventListener('change', () => {
   createGrid(defaultSize);
 });
 
-function createTrail(defaultSize, currentXPosition, currentYPosition) {
-
-}
-
 gridContainer.addEventListener('mousemove', event => {
-  event.target.classList.add('trail');
+ if (eventFire == true) {
+    let colorChoice = Math.floor(Math.random() * 5);
+    event.target.style.backgroundColor = `${MULTI_COLOURS[colorChoice]}`;
+ }
+else {
+    event.target.style.backgroundColor = `${MULTI_COLOURS[0]}`;
+  }
+});
+
+eraserButton.addEventListener('click', clearScreen);
+
+multiColorsCheck.addEventListener('change',(event) => {
+  if (event.target.checked) {
+    eventFire = true;
+  }
+  else {
+    eventFire = false;
+  }
 });
 
 
